aa138533f88de03367bd4aa64d290c6c8100c6e5
Ikhsan310105 <ini.ikhsan31@gmail.com>
2023-09-27 10:54:00 +0700

    admin ui

--- frontend-myroti/src/components/header/AppHeaderDropdown.js
+++ frontend-myroti/src/components/header/AppHeaderDropdown.js
@@ -33,8 +33,6 @@ const AppHeaderDropdown = () => {
       <CDropdownMenu className="pt-0" placement="bottom-end">
         <CDropdownHeader className="bg-light fw-semibold py-2">Account</CDropdownHeader>
         <CDropdownItem href="#">
-          <CIcon icon={cilBell} className="me-2" />
-          Updates
           <CBadge color="info" className="ms-2">
             42
           </CBadge>
