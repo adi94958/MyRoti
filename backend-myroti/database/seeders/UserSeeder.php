658ca75ebdb333d971576aeecc09157df1c18a30
Alya_Angraini <116321705+aruya2707@users.noreply.github.com>
2023-10-04 10:22:53 +0700

    up userseed

--- backend-myroti/database/seeders/UserSeeder.php
+++ backend-myroti/database/seeders/UserSeeder.php
@@ -15,41 +15,50 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // DB::table('admins')->insert([
-        //     'username' => 'admin',
-        //     'password' => Hash::make('123456'),
-        //     'user_type' => 'admin',
-        //     'created_at' => Carbon::now(),
-        //     'updated_at' => Carbon::now(),
-        // ]);
+        DB::table('admins')->insert([
+            'username' => 'aruya',
+            'password' => Hash::make('aruya123'),
+            'user_type' => 'admin',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
         DB::table('koordinators')->insert([
+            [
             'username' => 'koordinator',
             'password' => Hash::make('123'),
             'nama' => 'Adi',
             'user_type' => 'koordinator',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
+        ],
+        [
+            'username' => 'koordinator2',
+            'password' => Hash::make('456'),
+            'nama' => 'Alya',
+            'user_type' => 'koordinator',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]
         ]);
-        // DB::table('kurirs')->insert([
-        //     [
-        //         'username' => 'kurir2',
-        //         'password' => Hash::make('123456'),
-        //         'nama' => 'Adrian',
-        //         'area' => 'Bandung Barat',
-        //         'user_type' => 'kurir',
-        //         'created_at' => now(),
-        //         'updated_at' => now(),
-        //     ],
-        //     [
-        //         'username' => 'kurir3',
-        //         'password' => Hash::make('123456'),
-        //         'nama' => 'Fadel',
-        //         'area' => 'Cimahi',
-        //         'user_type' => 'kurir',
-        //         'created_at' => now(),
-        //         'updated_at' => now(),
-        //     ]
-        // ]);
-        
+        DB::table('kurirs')->insert([
+            [
+                'username' => 'kurir2',
+                'password' => Hash::make('123456'),
+                'nama' => 'Adrian',
+                'area' => 'Bandung Barat',
+                'user_type' => 'kurir',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'username' => 'kurir3',
+                'password' => Hash::make('123456'),
+                'nama' => 'Fadel',
+                'area' => 'Cimahi',
+                'user_type' => 'kurir',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
     }
 }
